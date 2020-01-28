ed23eac4f285487886d5d6a9d360ce292e00440e
abelvalle85 <abel.valle@scalablepress.com>
2020-01-28 11:13:03 -0600

    Editting workFlow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -12,7 +12,7 @@ def call(Map pipelineParams) {
             }
         }*/
 
-        parameters {
+       /* parameters {
             choice(name: 'ENV', choices: ['Stage','Production'], description: 'Select the Environment for build and deploy')
             choice(name: 'PROJECT', choices: ['sl','inkpop'], description: 'Select the project to use')
             booleanParam(name:'PULL', defaultValue: true, description: 'git pull required')
@@ -20,7 +20,7 @@ def call(Map pipelineParams) {
             string(name: 'DOCKER_TAG', defaultValue: 'development', description: 'DOCKER tag to use to reference the image')
             booleanParam(name:'START_CONTAINER', defaultValue: true, description: 'Start the container on the target')
             choice(name: 'SERVER_FAMILY', choices: ['pajamas-all','pajamas-1'], description: 'Select servers')
-        }
+        }*/
         stages {
             stage("Pre-build") {
                 steps {

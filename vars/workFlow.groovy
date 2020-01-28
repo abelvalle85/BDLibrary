bde2995d03cd867074eafd7e7391ca7e2fc0e7c3
abelvalle85 <abel.valle@scalablepress.com>
2020-01-28 15:46:45 -0600

    Editting workFlow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -14,14 +14,32 @@ def call(Map pipelineParams) {
         }*/
 
         parameters {
-            choice(name: 'ENV', choices: ['Stage','Production'], description: 'Select the Environment for build and deploy')
+            activeChoiceParam("ENV"){
+                description('Select the environment')
+                choiceType('SINGLE_SELECT')
+                groovyScript {
+                    script('["Stage","Production"]')
+                    fallbackScript('return ["ERROR"]')
+                }
+            }
+            activeChoiceReactiveParam('Family_Servers'){
+                description('Select server for deploy')
+                filterable()
+                choiceType('SINGLE_SELECT')
+                groovyScript {
+                    fillChoices("../resources/${params.ENV}Server.txt")
+                    fallbackScript('return ["Script error!"]')
+                }
+                referencedParameter('ENV')
+            }
+            /*choice(name: 'ENV', choices: ['Stage','Production'], description: 'Select the Environment for build and deploy')
             choice(name: 'PROJECT', choices: ['sl','inkpop'], description: 'Select the project to use')
             booleanParam(name:'PULL', defaultValue: true, description: 'git pull required')
             string(name: 'BRANCH', defaultValue: 'development', description: 'Add code branch')
             string(name: 'DOCKER_TAG', defaultValue: 'development', description: 'DOCKER tag to use to reference the image')
             booleanParam(name:'START_CONTAINER', defaultValue: true, description: 'Start the container on the target')
             choice(name: 'SERVER_FAMILY', choices: ['pajamas-all','pajamas-1'], description: 'Select servers')
-            booleanParam(name:'BUILD', defaultValue: true, description: 'git pull required')
+            booleanParam(name:'BUILD', defaultValue: true, description: 'git pull required')*/
         }
         stages {
             stage("Pre-build") {

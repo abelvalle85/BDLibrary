e4545eeb0e7d71e7adfd8dd7be0836c6750363ff
abelvalle85 <abel.valle@scalablepress.com>
2020-01-29 11:07:24 -0600

    Editting workFlow groovy file

--- vars/fillChoices.groovy
+++ vars/fillChoices.groovy
@@ -8,5 +8,8 @@ def call(String filepath){
     new File(filepath).eachLine { line ->
         parameters << line
     }
-    return parameters
+    //return parameters
+    parameters.each {
+        println it
+    }
 }

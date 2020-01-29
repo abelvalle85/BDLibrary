2e9085b5e3b4c09172698067b4a012793d0c64a5
abelvalle85 <abel.valle@scalablepress.com>
2020-01-29 16:10:57 -0600

    Editting workflow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -58,7 +58,7 @@ def call(Map pipelineParams) {
                                                         servers << line
                                                     }
                                                     
-                                                    return [servers]
+                                                    return servers
                                 //return[\'devaaa001\',\'devaaa002\',\'devbbb001\',\'devbbb002\',\'devccc001\',\'devccc002\']
                                 //def stageServers = fillChoices("${get_resource_dir()}/StageServers.txt")
                                 //'return[fillChoices("${get_resource_dir()}/StageServers.txt")]'

13f2ac6971e2000674f35bae6911a8dec1eeb236
abelvalle85 <abel.valle@scalablepress.com>
2020-01-30 16:07:26 -0600

    Editting fill choices groovy file

--- vars/fillChoices.groovy
+++ vars/fillChoices.groovy
@@ -1,23 +1,7 @@
 def call(String path){
-   /* List<String> readFileInList(String filePath) {
-        File file = new File(filePath)
-        def lines = file.readLines()
-        return lines
-    }*/
-    //def StageServers = ['pajamas-all','pajamas-1','pajamas-3','pajamas-4']
-
-    //def ProductionServers = ['tuxedo-all','tuxedo-1','tuxedo-2','tuxedo-3','tuxedo-4']
-    def lines=[]
-    File file=new File(path)
-    lines=file.readLines()
-    return lines
-    /*new File(filepath).eachLine { line ->
-        parameters << line
+    def servers=[]
+    new File(path).eachLine{ line->
+        servers << line
     }
-    return parameters*/
-    /*parameters.each {
-        println it
-    }*/
-
-
+    return  servers
 }

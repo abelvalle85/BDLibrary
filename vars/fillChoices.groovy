9642ebbf8f9867451b46368aa745a51c41ee8b61
abelvalle85 <abel.valle@scalablepress.com>
2020-01-30 15:32:35 -0600

    Editting fill choices groovy file

--- vars/fillChoices.groovy
+++ vars/fillChoices.groovy
@@ -5,10 +5,11 @@ def call(Map f){
         return lines
     }*/
     //def StageServers = ['pajamas-all','pajamas-1','pajamas-3','pajamas-4']
-
+    println f.ENV
+    println "{f.ENV}"
     //def ProductionServers = ['tuxedo-all','tuxedo-1','tuxedo-2','tuxedo-3','tuxedo-4']
     def lines=[]
-    File file=new File("${get_resource_dir()}/${f.ENV}Servers.txt")
+    File file=new File("${get_resource_dir()}/{f.ENV}Servers.txt")
     lines=file.readLines()
     return lines
     /*new File(filepath).eachLine { line ->

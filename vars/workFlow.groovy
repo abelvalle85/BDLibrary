e20eb2ff8ae82dd07080b1eef2160f5c04d95f1e
abelvalle85 <abel.valle@scalablepress.com>
2020-01-30 11:02:19 -0600

    Editting workflow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -27,7 +27,7 @@ def call(Map pipelineParams) {
                                      classpath: [],
                                      sandbox: true,
                                      script: '''
-                                             def type = ["Stage1","Production1"]
+                                             def type = ["Stage","Production"]
                                              return type
                                         '''
 

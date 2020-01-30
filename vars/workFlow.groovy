7b9e6cc749d25d27d48c16e63259634f93f82ee9
abelvalle85 <abel.valle@scalablepress.com>
2020-01-29 18:36:34 -0600

    Editting workflow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -58,9 +58,9 @@ def call(Map pipelineParams) {
                                      classpath: [],
                                      sandbox: true,
                                      script: '''
-                                          def listS = fillChoices("${get_resource_dir()}/${params.Env}Servers.txt")
+                                          
                                           //return ["pajamas-all", "pajamas-1", "pajamas-2", "pajamas-3", "pajamas-4", "pajamas-5"]
-                                          return listS
+                                          return fillChoices("${get_resource_dir()}/${params.Env}Servers.txt")
                                  '''
 
                              ]

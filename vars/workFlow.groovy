fa64cc0afa476f8de7988dd2d4a5dd36f37d4b4f
abelvalle85 <abel.valle@scalablepress.com>
2020-01-30 14:10:46 -0600

    Editting workflow groovy file

--- vars/workFlow.groovy
+++ vars/workFlow.groovy
@@ -60,7 +60,7 @@ def call(Map pipelineParams) {
                                      script: """//'''
                                           if (Env.equals('Stage')) {
                                               def servers = []
-                                              servers = server.plus(fillChoices(\\"${get_resource_dir()}/${params.Env}Servers.txt\\"))
+                                              servers = server.plus(fillChoices(\"${get_resource_dir()}/${params.Env}Servers.txt\"))
                                               return servers //fillChoices(\"${get_resource_dir()}/${params.Env}Servers.txt\")
                                             } else if (Env.equals('Production')) {
                                               return fillChoices(\"${get_resource_dir()}/${params.Env}Servers.txt\")
